badf1aa8c64d468bd38228071f3a9c10b126308e
jamydevelop <127085144+jamydevelop@users.noreply.github.com>
2025-07-20 02:30:16 +0800

    .

--- mvvm_flutter_with_provider/lib/main.dart
+++ mvvm_flutter_with_provider/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mvvm_flutter_with_provider/home_screen.dart';
+import 'package:mvvm_flutter_with_provider/users_list/view_models/users_view_model.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-
+        ChangeNotifierProvider(create: (_) => UsersViewModel())
       ],
       child: MaterialApp(
       title: 'MVVM',

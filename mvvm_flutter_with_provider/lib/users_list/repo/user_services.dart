764b3742e43e6d117bca0344e4d272dc8113a1a8
jamydevelop <127085144+jamydevelop@users.noreply.github.com>
2025-07-21 23:12:57 +0800

    added headers

--- mvvm_flutter_with_provider/lib/users_list/repo/user_services.dart
+++ mvvm_flutter_with_provider/lib/users_list/repo/user_services.dart
@@ -7,25 +7,29 @@ import 'package:mvvm_flutter_with_provider/utils/constants.dart';
 
 class UserServices {
 
-  //returning Object, since we are going to return multiple of object.
-  static Future<Object> getUsers() async{
+  // Returning Object, since we are going to return multiple of object.
+  static Future<Object> getUsers() async {
     try {
-
       var url = Uri.parse(USERS_LIST);
-      var response = await http.get(url);
+      var headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        // 'Authorization': 'Bearer YOUR_TOKEN_HERE', // Uncomment if needed
+      };
 
-      if(200 == response.statusCode) {
+      var response = await http.get(url, headers: headers);
+
+      if (response.statusCode == 200) {
         return Success(response: usersListModelFromJson(response.body));
       }
 
       return Failure(code: USER_INVALID_RESPONSE, errorResponse: 'Invalid Response');
-    } on HttpException {
-       return Failure(code: NO_INTERNET, errorResponse: 'No Internet');
-    } on HttpException {
-       return Failure(code: INVALID_FORMAT, errorResponse: 'Invalid Format');
+    } on SocketException {
+      return Failure(code: NO_INTERNET, errorResponse: 'No Internet');
+    } on FormatException {
+      return Failure(code: INVALID_FORMAT, errorResponse: 'Invalid Format');
     } catch (e) {
-       return Failure(code: UNKNOWN_ERROR, errorResponse: 'Unkown Error');
+      return Failure(code: UNKNOWN_ERROR, errorResponse: 'Unknown Error');
     }
-
   }
 }

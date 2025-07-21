26451b4ef9029ef68ef11c7ee45a82986b46d041
jamydevelop <127085144+jamydevelop@users.noreply.github.com>
2025-07-21 23:02:06 +0800

    misc

--- mvvm_flutter_with_provider/lib/users_list/repo/user_services.dart
+++ mvvm_flutter_with_provider/lib/users_list/repo/user_services.dart
@@ -1,3 +1,34 @@
+// import 'dart:io';
+
+// import 'package:http/http.dart' as http;
+// import 'package:mvvm_flutter_with_provider/users_list/models/users_list_model.dart';
+// import 'package:mvvm_flutter_with_provider/users_list/repo/api_status.dart';
+// import 'package:mvvm_flutter_with_provider/utils/constants.dart';
+
+// class UserServices {
+
+//   //returning Object, since we are going to return multiple of object.
+//   static Future<Object> getUsers() async{
+//     try {
+
+//       var url = Uri.parse(USERS_LIST);
+//       var response = await http.get(url);
+
+//       if(200 == response.statusCode) {
+//         return Success(response: usersListModelFromJson(response.body));
+//       }
+
+//       return Failure(code: USER_INVALID_RESPONSE, errorResponse: 'Invalid Response');
+//     } on HttpException {
+//        return Failure(code: NO_INTERNET, errorResponse: 'No Internet');
+//     } on HttpException {
+//        return Failure(code: INVALID_FORMAT, errorResponse: 'Invalid Format');
+//     } catch (e) {
+//        return Failure(code: UNKNOWN_ERROR, errorResponse: 'Unkown Error');
+//     }
+
+//   }
+// }
 import 'dart:io';
 
 import 'package:http/http.dart' as http;
@@ -6,26 +37,34 @@ import 'package:mvvm_flutter_with_provider/users_list/repo/api_status.dart';
 import 'package:mvvm_flutter_with_provider/utils/constants.dart';
 
 class UserServices {
-
-  //returning Object, since we are going to return multiple of object.
-  static Future<Object> getUsers() async{
+  // Returning Object, since we are going to return multiple types of objects.
+  static Future<Object> getUsers() async {
     try {
-
       var url = Uri.parse(USERS_LIST);
-      var response = await http.get(url);
 
-      if(200 == response.statusCode) {
+      var response = await http.get(
+        url,
+        headers: {
+          'User-Agent':
+              'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
+          'Accept': 'application/json',
+        },
+      );
+
+      if (response.statusCode == 200) {
         return Success(response: usersListModelFromJson(response.body));
       }
 
-      return Failure(code: USER_INVALID_RESPONSE, errorResponse: 'Invalid Response');
+      return Failure(
+        code: USER_INVALID_RESPONSE,
+        errorResponse: 'Invalid Response',
+      );
+    } on SocketException {
+      return Failure(code: NO_INTERNET, errorResponse: 'No Internet');
     } on HttpException {
-       return Failure(code: NO_INTERNET, errorResponse: 'No Internet');
-    } on HttpException {
-       return Failure(code: INVALID_FORMAT, errorResponse: 'Invalid Format');
+      return Failure(code: INVALID_FORMAT, errorResponse: 'Invalid Format');
     } catch (e) {
-       return Failure(code: UNKNOWN_ERROR, errorResponse: 'Unkown Error');
+      return Failure(code: UNKNOWN_ERROR, errorResponse: 'Unknown Error');
     }
-
   }
 }
